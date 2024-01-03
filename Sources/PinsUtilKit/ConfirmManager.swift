0476af2cf83993686e4879954c7cf9d43f087350
MojitoBar <wnehdtjr5@gmail.com>
2024-01-03 22:16:59 +0900

    디스크 캐싱 & 컨펌 ㅁ추가

---
+++ Sources/PinsUtilKit/ConfirmManager.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by 주동석 on 1/3/24.
+//
+
+import UIKit
+enum ConfirmManager {
+    static func makeAlert(title: String, message: String, action: @escaping () -> Void) -> UIAlertController {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let confirmAction = UIAlertAction(title: "확인", style: .default) { _ in
+            action()
+        }
+        alert.addAction(confirmAction)
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel)
+        alert.addAction(cancelAction)
+        
+        return alert
+    }
+}
